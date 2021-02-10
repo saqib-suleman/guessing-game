0bcd53fe22462b6bae55aad9740567cf9b21e118
Saqib Suleman <saqibs3291@gmail.com>
2021-02-10 11:56:30 +0500

    completed

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ while (!maximum) {
     maximum = parseInt(prompt("Enter a valid number!"))
 }
 const targetNum = Math.floor(Math.random() * maximum) + 1;
-// console.log(targetNum);
 
 let guess = parseInt(prompt("Enter your first guess!"));
-while (guess !== targetNum) {
+let attempts = 1;
+while (parseInt(guess) !== targetNum) {
+    if (guess === "q") break;
+    attempts++;
     if (guess > targetNum) {
-        guess = parseInt(prompt("Too high! Enter a new guess"));
+        guess = prompt("Too high! Enter a new guess");
     } else {
-        guess = parseInt(prompt("Too low! Enter a new guess"));
+        guess = prompt("Too low! Enter a new guess");
     }
 }
-console.log("You win!")
+
+if (guess === "q") {
+    console.log("OK, YOU QUIT!")
+} else {
+    console.log(`You got it. It took you ${attempts} guess(es).`)
+}
