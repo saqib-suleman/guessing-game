1f19473b3346176e1aa946dbce8af3e6d3231f8a
Saqib Suleman <saqibs3291@gmail.com>
2021-02-10 11:40:13 +0500

    add while loop

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 let maximum = parseInt(prompt("Enter a maximum number!"));
-
-if (!maximum) {
+while (!maximum) {
     maximum = parseInt(prompt("Enter a valid number!"))
 }
-
 const targetNum = Math.floor(Math.random() * maximum) + 1;
-console.log(targetNum);
+// console.log(targetNum);
+
+let guess = parseInt(prompt("Enter your first guess!"));
+while (guess !== targetNum) {
+    if (guess > targetNum) {
+        guess = parseInt(prompt("Too high! Enter a new guess"));
+    } else {
+        guess = parseInt(prompt("Too low! Enter a new guess"));
+    }
+}
+console.log("You win!")
